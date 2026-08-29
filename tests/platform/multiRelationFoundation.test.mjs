38a77d459d037f2078ab69bc58b3aee1991e5ec6
matthewhawkinsus1-crypto <matthewhawkinsus1@gmail.com>
2026-08-29 16:03:25 -0500

    Fix multi-relation regression test import

--- tests/platform/multiRelationFoundation.test.mjs
+++ tests/platform/multiRelationFoundation.test.mjs
@@ -13,6 +13,7 @@ import {
   needsMultiRelationWorkspace,
   parseRelationSource,
   relationContainsInvisibleNegativeAbsolute,
+  relationExpressionsEquivalent,
   relationSolutionSummary,
   relationStateContainsAbsoluteValue,
   relationStateToText,
