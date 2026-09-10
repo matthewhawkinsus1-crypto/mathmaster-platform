554b153f436b8c3e8eac4d7c25b4239412c68d05
matthewhawkinsus1-crypto <matthewhawkinsus1@gmail.com>
2026-09-10 15:56:59 -0500

    feat: expose assignment content lineage helpers

---
+++ src/platform/assignments/assignmentContentVersion.js
@@ -0,0 +1,8 @@
+export {
+  contentVersionLabel,
+  contentVersionOf,
+  groupCurrentLibraryReleases,
+  latestFamilyRelease,
+  normalizeContentLineage,
+  sameContentFamily,
+} from '../../../functions/shared/assignmentContentVersion.mjs';
