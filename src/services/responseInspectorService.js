15154f5d50b138c9b7e0eef71b37701af042dd6d
matthewhawkinsus1-crypto <matthewhawkinsus1@gmail.com>
2026-09-15 22:18:29 -0500

    Add response inspector client service

---
+++ src/services/responseInspectorService.js
@@ -0,0 +1,12 @@
+import { httpsCallable } from 'firebase/functions';
+import { functions } from '../firebase.js';
+
+export const inspectStudentResponse = async (identity) => {
+  const result = await httpsCallable(functions, 'inspectStudentResponse')(identity);
+  return result.data;
+};
+
+export const overrideStudentResponseGrade = async (request) => {
+  const result = await httpsCallable(functions, 'overrideStudentResponseGrade')(request);
+  return result.data;
+};
